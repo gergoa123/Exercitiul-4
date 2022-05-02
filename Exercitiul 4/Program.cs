348360c1a2dbb53e5fa6a3c3151a2512127b2019
Gergoa123 <gergo3124@gmail.com>
2022-05-02 18:16:40 +0300

    Add project files.

---
+++ Exercitiul 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercitiul_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul!");
+            int numar = Convert.ToInt32(Console.ReadLine());
+
+            if (numar > 0 ) 
+            {
+                Console.WriteLine(" + ");
+            }
+
+            if (numar < 0) 
+            {
+                Console.WriteLine(" - ");
+            }
+
+            if (numar == 0) 
+            {
+                Console.WriteLine(" 0 ");
+            }
+        }
+    }
+}
